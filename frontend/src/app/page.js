28304b9ab71e75cbcc0c4c9fbbba775f4212307b
Wojciech Kowal <w.kowal202@gmail.com>
2024-12-31 14:02:48 +0100

    Improve search form

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,17 @@
 "use client";
 
-import { Autocomplete, AutocompleteItem, Button, Form, Input, Select, SelectItem } from "@nextui-org/react";
+import {
+	Autocomplete,
+	AutocompleteItem,
+	Button,
+	Form,
+	Input,
+} from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { API_URL } from "./config";
-import { useRouter } from "next/navigation";
+import { redirect, usePathname, useSearchParams } from "next/navigation";
 
-export async function fetchQueryParameters() {
+export async function fetchAutocompleteValues() {
 	const endpoints = [
 		"/api/miejscowosci",
 		"/api/wojewodztwa",
@@ -29,34 +35,77 @@ export async function fetchQueryParameters() {
 }
 
 export default function Home() {
-	const router = useRouter();
-
-	const [queryParameters, setQueryParameters] = useState({});
-	const [submitted, setSubmitted] = useState(false);
-
-	const [searchQuery, setSearchQuery] = useState("");
-	const [miejscowosc, setMiejscowosc] = useState();
-	const [wojewodztwo, setWojewodztwo] = useState();
-	const [powiat, setPowiat] = useState();
-	const [gmina, setGmina] = useState();
-	const [typ_podmiotu, setTypPodmiotu] = useState();
-	const [rodzaj_placowki, setRodzajPlacowki] = useState();
-	const [specyfika_szkoly, setSpecyfikaSzkoly] = useState();
-	const [rodzaj_publicznosci, setRodzajPublicznosci] = useState();
-
-	const [searchResults, setSearchResults] = useState({});
-
+	// get values for autocomplete elements
+	const [autocompleteValues, setAutocompleteValues] = useState({});
 	useEffect(() => {
 		(async () => {
-			setQueryParameters(await fetchQueryParameters());
+			setAutocompleteValues(await fetchAutocompleteValues());
 		})();
 	}, []);
+
+	// get pathname and search params
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+
+	// handle searchParams change
+	const [searchResults, setSearchResults] = useState({});
+	useEffect(() => {
+		if (searchParams.size < 1) return;
+
+		(async () => {
+			setSubmitted(true);
+
+			const response = await fetch(
+				API_URL + `/api/search?${searchParams.toString()}`,
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+
+			const data = await response.json();
+
+			if (response.ok) {
+				console.log(data.search_results);
+			} else {
+				console.warn(data.error);
+			}
+
+			setSubmitted(false);
+		})();
+	}, [searchParams]);
+
+	// controlled form madness
+	const [submitted, setSubmitted] = useState(false);
+	const [searchQuery, setSearchQuery] = useState(searchParams.get("query"));
+	const [miejscowosc, setMiejscowosc] = useState(
+		searchParams.get("miejscowosc")
+	);
+	const [wojewodztwo, setWojewodztwo] = useState(
+		searchParams.get("wojewodztwo")
+	);
+	const [powiat, setPowiat] = useState(searchParams.get("powiat"));
+	const [gmina, setGmina] = useState(searchParams.get("gmina"));
+	const [typ_podmiotu, setTypPodmiotu] = useState(
+		searchParams.get("typ_podmiotu")
+	);
+	const [rodzaj_placowki, setRodzajPlacowki] = useState(
+		searchParams.get("rodzaj_placowki")
+	);
+	const [specyfika_szkoly, setSpecyfikaSzkoly] = useState(
+		searchParams.get("specyfika_szkoly")
+	);
+	const [rodzaj_publicznosci, setRodzajPublicznosci] = useState(
+		searchParams.get("rodzaj_publicznosci")
+	);
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
 		const params = {
-			query: searchQuery,
+			query: searchQuery || "",
 			miejscowosc: miejscowosc || "",
 			wojewodztwo: wojewodztwo || "",
 			powiat: powiat || "",
@@ -64,186 +113,183 @@ export default function Home() {
 			typ_podmiotu: typ_podmiotu || "",
 			rodzaj_placowki: rodzaj_placowki || "",
 			specyfika_szkoly: specyfika_szkoly || "",
-			rodzaj_publicznosci:
-				rodzaj_publicznosci || "",
+			rodzaj_publicznosci: rodzaj_publicznosci || "",
 		};
 
-		setSubmitted(true);
-
-		const response = await fetch(
-			API_URL + `/api/search?${new URLSearchParams(params).toString()}`,
-			{
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}
-		);
-
-		const data = await response.json();
-
-		if (response.ok) {
-			console.log(data.search_results);
-		} else {
-			console.warn(data.error);
-		}
-
-		setSubmitted(false);
+		redirect(`${pathname}?${new URLSearchParams(params).toString()}`);
 	};
 
 	return (
-		<Form
-			className="w-11/12 md:w-9/12 lg:w-7/12 m-auto pt-16"
-			onSubmit={handleSubmit}
-		>
-			<div className="flex flex-nowrap gap-4 w-full pt-4">
-				<Input
-					className="w-10/12"
-					isClearable
-					size="md"
-					label="Nazwa placówki"
-					labelPlacement="inside"
-					name="search_query"
-					type="text"
-					value={searchQuery}
-					onValueChange={setSearchQuery}
-					variant="faded"
-					color="default"
-				/>
-				<Button
-					className="w-2/12 m-auto"
-					type="submit"
-					size="lg"
-					color="primary"
-					variant="shadow"
-					isLoading={submitted}
-				>
-					Szukaj
-				</Button>
-			</div>
-			<div className="flex flex-nowrap gap-4 w-full pt-4">
-				<Autocomplete
-					selectedKeys={miejscowosc}
-					onSelectionChange={setMiejscowosc}
-					className="w-1/4"
-					color="primary"
-					labelPlacement="inside"
-					scrollShadowProps={{
-						isEnabled: false,
-					}}
-					label="Miejscowość"
-				>
-					{queryParameters.miejscowosci?.map((typ) => (
-						<AutocompleteItem key={typ.id}>{typ.nazwa}</AutocompleteItem>
-					))}
-				</Autocomplete>
-				<Autocomplete
-					selectedKeys={wojewodztwo}
-					onSelectionChange={setWojewodztwo}
-					className="w-1/4"
-					color="primary"
-					labelPlacement="inside"
-					scrollShadowProps={{
-						isEnabled: false,
-					}}
-					label="Województwo"
-				>
-					{queryParameters.wojewodztwa?.map((typ) => (
-						<AutocompleteItem key={typ.id}>{typ.nazwa}</AutocompleteItem>
-					))}
-				</Autocomplete>
-				<Autocomplete
-					selectedKeys={gmina}
-					onSelectionChange={setGmina}
-					className="w-1/4"
-					color="primary"
-					labelPlacement="inside"
-					scrollShadowProps={{
-						isEnabled: false,
-					}}
-					label="Gmina"
-				>
-					{queryParameters.gminy?.map((typ) => (
-						<AutocompleteItem key={typ.id}>{typ.nazwa}</AutocompleteItem>
-					))}
-				</Autocomplete>
-				<Autocomplete
-					selectedKeys={powiat}
-					onSelectionChange={setPowiat}
-					className="w-1/4"
-					color="primary"
-					labelPlacement="inside"
-					scrollShadowProps={{
-						isEnabled: false,
-					}}
-					label="Powiat"
-				>
-					{queryParameters.powiaty?.map((typ) => (
-						<AutocompleteItem key={typ.id}>{typ.nazwa}</AutocompleteItem>
-					))}
-				</Autocomplete>
-			</div>
+		<>
+			<Form
+				className="w-11/12 md:w-9/12 lg:w-7/12 m-auto pt-16"
+				onSubmit={handleSubmit}
+			>
+				<div className="flex flex-nowrap gap-4 w-full pt-4">
+					<Input
+						className="w-10/12"
+						isClearable
+						size="md"
+						label="Nazwa placówki"
+						labelPlacement="inside"
+						name="search_query"
+						type="text"
+						value={searchQuery}
+						onValueChange={setSearchQuery}
+						variant="faded"
+						color="default"
+					/>
+					<Button
+						className="w-2/12 m-auto"
+						type="submit"
+						size="lg"
+						color="primary"
+						variant="shadow"
+						isLoading={submitted}
+					>
+						Szukaj
+					</Button>
+				</div>
+				<div className="flex flex-nowrap gap-4 w-full pt-4">
+					<Autocomplete
+						selectedKey={miejscowosc}
+						onSelectionChange={setMiejscowosc}
+						className="w-1/4"
+						color="primary"
+						labelPlacement="inside"
+						scrollShadowProps={{
+							isEnabled: false,
+						}}
+						label="Miejscowość"
+					>
+						{autocompleteValues.miejscowosci?.map((typ) => (
+							<AutocompleteItem key={typ.id}>
+								{typ.nazwa}
+							</AutocompleteItem>
+						))}
+					</Autocomplete>
+					<Autocomplete
+						selectedKey={wojewodztwo}
+						onSelectionChange={setWojewodztwo}
+						className="w-1/4"
+						color="primary"
+						labelPlacement="inside"
+						scrollShadowProps={{
+							isEnabled: false,
+						}}
+						label="Województwo"
+					>
+						{autocompleteValues.wojewodztwa?.map((typ) => (
+							<AutocompleteItem key={typ.id}>
+								{typ.nazwa}
+							</AutocompleteItem>
+						))}
+					</Autocomplete>
+					<Autocomplete
+						selectedKey={gmina}
+						onSelectionChange={setGmina}
+						className="w-1/4"
+						color="primary"
+						labelPlacement="inside"
+						scrollShadowProps={{
+							isEnabled: false,
+						}}
+						label="Gmina"
+					>
+						{autocompleteValues.gminy?.map((typ) => (
+							<AutocompleteItem key={typ.id}>
+								{typ.nazwa}
+							</AutocompleteItem>
+						))}
+					</Autocomplete>
+					<Autocomplete
+						selectedKey={powiat}
+						onSelectionChange={setPowiat}
+						className="w-1/4"
+						color="primary"
+						labelPlacement="inside"
+						scrollShadowProps={{
+							isEnabled: false,
+						}}
+						label="Powiat"
+					>
+						{autocompleteValues.powiaty?.map((typ) => (
+							<AutocompleteItem key={typ.id}>
+								{typ.nazwa}
+							</AutocompleteItem>
+						))}
+					</Autocomplete>
+				</div>
 
-			<div className="flex flex-nowrap gap-4 w-full pt-4">
-				<Autocomplete
-					selectedKeys={typ_podmiotu}
-					onSelectionChange={setTypPodmiotu}
-					className="w-3/6"
-					labelPlacement="inside"
-					scrollShadowProps={{
-						isEnabled: false,
-					}}
-					label="Typ podmiotu"
-				>
-					{queryParameters.typy_podmiotow?.map((typ) => (
-						<AutocompleteItem key={typ.id}>{typ.nazwa}</AutocompleteItem>
-					))}
-				</Autocomplete>
-				<Autocomplete
-					selectedKeys={rodzaj_placowki}
-					onSelectionChange={setRodzajPlacowki}
-					className="w-3/6"
-					labelPlacement="inside"
-					scrollShadowProps={{
-						isEnabled: false,
-					}}
-					label="Rodzaj placówki"
-				>
-					{queryParameters.rodzaje_placowek?.map((typ) => (
-						<AutocompleteItem key={typ.id}>{typ.nazwa}</AutocompleteItem>
-					))}
-				</Autocomplete>
-			</div>
-			<div className="flex flex-nowrap gap-4 w-full">
-				<Autocomplete
-					selectedKeys={specyfika_szkoly}
-					onSelectionChange={setSpecyfikaSzkoly}
-					className="w-3/12"
-					labelPlacement="inside"
-					scrollShadowProps={{
-						isEnabled: false,
-					}}
-					label="Specyfika szkoły"
-				>
-					{queryParameters.specyfiki_szkol?.map((typ) => (
-						<AutocompleteItem key={typ.id}>{typ.nazwa}</AutocompleteItem>
-					))}
-				</Autocomplete>
-				<Autocomplete
-					selectedKeys={rodzaj_publicznosci}
-					onSelectionChange={setRodzajPublicznosci}
-					className="w-9/12"
-					labelPlacement="inside"
-					scrollShadowProps={{
-						isEnabled: false,
-					}}
-					label="Rodzaj publiczności"
-				>
-					{queryParameters.rodzaje_publicznosci?.map((typ) => (
-						<AutocompleteItem key={typ.id}>{typ.nazwa}</AutocompleteItem>
-					))}
-				</Autocomplete>
-			</div>
-		</Form>
+				<div className="flex flex-nowrap gap-4 w-full pt-4">
+					<Autocomplete
+						selectedKey={typ_podmiotu}
+						onSelectionChange={setTypPodmiotu}
+						className="w-3/6"
+						labelPlacement="inside"
+						scrollShadowProps={{
+							isEnabled: false,
+						}}
+						label="Typ podmiotu"
+					>
+						{autocompleteValues.typy_podmiotow?.map((typ) => (
+							<AutocompleteItem key={typ.id}>
+								{typ.nazwa}
+							</AutocompleteItem>
+						))}
+					</Autocomplete>
+					<Autocomplete
+						selectedKey={rodzaj_placowki}
+						onSelectionChange={setRodzajPlacowki}
+						className="w-3/6"
+						labelPlacement="inside"
+						scrollShadowProps={{
+							isEnabled: false,
+						}}
+						label="Rodzaj placówki"
+					>
+						{autocompleteValues.rodzaje_placowek?.map((typ) => (
+							<AutocompleteItem key={typ.id}>
+								{typ.nazwa}
+							</AutocompleteItem>
+						))}
+					</Autocomplete>
+				</div>
+				<div className="flex flex-nowrap gap-4 w-full">
+					<Autocomplete
+						selectedKey={specyfika_szkoly}
+						onSelectionChange={setSpecyfikaSzkoly}
+						className="w-3/12"
+						labelPlacement="inside"
+						scrollShadowProps={{
+							isEnabled: false,
+						}}
+						label="Specyfika szkoły"
+					>
+						{autocompleteValues.specyfiki_szkol?.map((typ) => (
+							<AutocompleteItem key={typ.id}>
+								{typ.nazwa}
+							</AutocompleteItem>
+						))}
+					</Autocomplete>
+					<Autocomplete
+						selectedKey={rodzaj_publicznosci}
+						onSelectionChange={setRodzajPublicznosci}
+						className="w-9/12"
+						labelPlacement="inside"
+						scrollShadowProps={{
+							isEnabled: false,
+						}}
+						label="Rodzaj publiczności"
+					>
+						{autocompleteValues.rodzaje_publicznosci?.map((typ) => (
+							<AutocompleteItem key={typ.id}>
+								{typ.nazwa}
+							</AutocompleteItem>
+						))}
+					</Autocomplete>
+				</div>
+			</Form>
+		</>
 	);
 }
